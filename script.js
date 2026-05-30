192b97705f5d5ab3ff26f49dd412e10f0af08faf
francisgul <francis17j.derco@gmail.com>
2026-05-30 03:21:57 +0000

    se modificaron algunos detalles

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const modal=document.getElementById('ventana-modal');
-const btnAbrir=document.getElementById('btn-abrir');
-const btnCerrar=document.getElementById('btn-cerrar');
+const ventana=document.getElementById('ventana-modal');
+const botonAbrir=document.getElementById('boton-abrir');
+const botonCerrar=document.getElementById('boton-cerrar');
 
-btnAbrir.addEventListener('click', () => {
-    modal.style.display='flex';
+botonAbrir.addEventListener('click', () => {
+    ventana.style.display='flex';
 });
 
-btnCerrar.addEventListener('click', () => {
-    modal.style.display='none';
+botonCerrar.addEventListener('click', () => {
+    ventana.style.display='none';
 });
 
 window.addEventListener('click', (evento) => {
-    if (evento.target===modal) {
-        modal.style.display='none';
+    if (evento.target===ventana) {
+        ventana.style.display='none';
     }
 });
 
@@ -24,21 +24,20 @@ document.getElementById('formulario-asesoria').addEventListener('submit', functi
     const telefono=document.getElementById('telefono').value.trim();
     const mensaje=document.getElementById('mensaje').value.trim();
 
-    // Validaciones de formato
-    const regexNombre=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(\s[a-zA-ZáéíóúÁÉÍÓÚñÑ]+)+$/;
-    if (!regexNombre.test(nombre)) {
+    const validarNombre=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(\s[a-zA-ZáéíóúÁÉÍÓÚñÑ]+)+$/;
+    if (!validarNombre.test(nombre)) {
         alert('Formato inválido: Ingresa tu nombre y apellido (solo letras).');
         return;
     }
 
-    const regexEmail=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regexEmail.test(email)) {
+    const validarEmail=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validarEmail.test(email)) {
         alert('Formato inválido: El correo electrónico no es correcto.');
         return;
     }
 
-    const regexTelefono = /^\d{9}$/;
-    if (!regexTelefono.test(telefono)){
+    const validarTelefono = /^\d{9}$/;
+    if (!validarTelefono.test(telefono)){
         alert('Formato inválido: El teléfono debe tener exactamente 9 dígitos.');
         return;
     }
@@ -73,5 +72,5 @@ document.getElementById('formulario-asesoria').addEventListener('submit', functi
     alert('¡Validación correcta! los datos ya fueron enviados.');
     
     document.getElementById('formulario-asesoria').reset();
-    modal.style.display='none';
+    ventana.style.display='none';
 });
